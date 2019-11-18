18126d0b6529ac2f6c37feedfb9ccc338cb2a0d6
wly2 <wly1-bk@qq.com>
2019-11-18 17:19:08 +0800

    泛型

---
+++ 01泛型/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01泛型
+{
+    class Class1<T>//T代表一个数据类型，当使用Class1进行构造的时候，需要指定T的类型
+    {
+        private T a;
+        private T b;
+
+        public Class1(T a, T b)
+        {
+            this.a = a;
+            this.b = b;
+
+        }
+
+        public string GetSum()
+        {
+            return "" + a + b;
+        }
+    }
+}
---
+++ 01泛型/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01泛型
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var aa = new Class1<string>("你好啊", "再见");//利用泛型类构造的时候，需要指定泛型的类型
+            var bb = new Class1<int>(1111, 2222);
+
+            //Console.WriteLine(aa.GetSum());
+            //Console.WriteLine(bb.GetSum());
+            Console.WriteLine(GetSum(1,2));
+
+            Console.ReadKey();
+        }
+
+        public static string GetSum<T>(T a, T b)
+        {
+            return a + "" + b;
+        }
+    }
+}
